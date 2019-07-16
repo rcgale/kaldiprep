964b6dc6c67f2f9bc97f5aa5b699b9b076467f9a
Robert Gale <galer@ohsu.edu>
2019-07-16 14:23:20 -0700

    Adding encoding to write_lexicon

--- kaldiprep/dictionary.py
+++ kaldiprep/dictionary.py
@@ -3,9 +3,9 @@ import os
 from kaldiprep.utterances import _write_file
 
 
-def write_lexicon(entries, path):
+def write_lexicon(entries, path, encoding=None):
     if not os.path.exists(path):
         os.makedirs(path)
 
     lexicon_lines = ["{} {}".format(word, pronunciation) for word, pronunciation in sorted(entries)]
-    _write_file("lexicon.txt", lexicon_lines, path)
+    _write_file("lexicon.txt", lexicon_lines, path, encoding=encoding)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='kaldiprep',
-    version='0.4.0',
+    version='0.4.1',
     author='Robert Gale',
     author_email='galer@ohsu.edu',
     packages=['kaldiprep'],
