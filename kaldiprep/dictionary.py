44b4cd2ea344cd02c429f3a0dccc7a57536af246
Robert Gale <galer@ohsu.edu>
2020-03-25 12:39:53 -0700

    Adding compatibility for multiple lexicon entries for the same word

--- kaldiprep/dictionary.py
+++ kaldiprep/dictionary.py
@@ -7,5 +7,20 @@ def write_lexicon(entries, path, encoding=None):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    lexicon_lines = ["{} {}".format(word, pronunciation) for word, pronunciation in sorted(entries)]
+    if isinstance(entries, dict):
+        entries = entries.items()
+
+    lexicon_lines = ["{} {}".format(word, pronunciation) for word, pronunciation in _iterate_entries(entries)]
     _write_file("lexicon.txt", lexicon_lines, path, encoding=encoding)
+
+
+def _iterate_entries(entries):
+    for word, pronunciation in sorted(entries):
+        if isinstance(pronunciation, str):
+            yield word, pronunciation
+
+        try:
+            for p in pronunciation:
+                yield word, p
+        except:
+            raise TypeError('Couldn\'t handle lexicon entry {} of type {}'.format(pronunciation, type(pronunciation)))
